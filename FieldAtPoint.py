438384e8e0675ceffc7772fae63f953709233b63
Jarvist Moore Frost <jarvist@gmail.com>
2016-01-22 20:24:10 +0000

    Makes a pretty arrows-Efield plot, and now forms contour on a slice in XY

--- FieldAtPoint.py
+++ FieldAtPoint.py
@@ -7,6 +7,7 @@ import NewPotentialModule as pot
 import math
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import colors,cm #colour maps; so I can specify cube helix
 import sys #for argv
 
 ## Input section (define the plane with 3 points, fractional coordinates)
@@ -48,21 +49,39 @@ b = pot.numbers_2_grid(b_point,NGX,NGY,NGZ)
 c = pot.numbers_2_grid(c_point,NGX,NGY,NGZ)
 plane_coeff = pot.points_2_plane(a,b,c)
 
-## Get the gradients
-XY = np.multiply(grad_x,grad_y)
-grad_mag = np.multiply(XY,grad_z)
+## Calculate magnitude of gradient.
+# Should be able to use numpy.linalg.norm for this, but the Python array indices are causing me grief
+X2 = np.multiply(grad_x,grad_x)
+Y2 = np.multiply(grad_y,grad_y)
+Z2 = np.multiply(grad_z,grad_z)
 
-## Create the plane
-xx,yy,grd =  pot.create_plotting_mesh(NGX,NGY,NGZ,plane_coeff,grad_y)
-## Plot the surface
-plt.contour(xx,yy,grd,20)
+grad_mag = np.sqrt(np.add(X2,Y2,Z2))
 
-plt.quiver(xx,yy,grad_x[:,:,NGZ/2],grad_y[:,:,NGZ/2] ,
-        color='blue'
+# This was my, non working, attempt to use the built in function.
+#grad_mag=np.linalg.norm( [grad_y,grad_y,grad_z], axis=3)
+
+## This function in Macrodensity averages Efield ACROSS Z for Slab calculations 
+#xx,yy,grd =  pot.create_plotting_mesh(NGX,NGY,NGZ,plane_coeff,grad_mag) #AVG over full volume
+
+# Here we construct the same xx,yy,grd variables with a SLICE, forming a plane in XY at particular ZSLICE
+xx, yy = np.mgrid[0:NGX,0:NGY]
+ZSLICE=0# NGZ/2
+grd=grad_mag[:,:,ZSLICE]
+# OK, that's all our data
+
+## Contours of the above sliced data
+plt.contour(xx,yy,grd,6,cmap=cm.cubehelix)
+
+# Also generate a set of Efield arrows ('quiver') for this data.
+# Specifying the drawing parameters is quite frustrating - they are very brittle + poorly documented.
+plt.quiver(xx,yy,grad_x[:,:,ZSLICE],grad_y[:,:,ZSLICE] ,
+        color='grey',
+        units='dots', width=1, headwidth=3, headlength=4
         ) #,
 #        units='xy', scale=10., zorder=3, color='blue',
 #        width=0.007, headwidth=3., headlength=4.)
 
+plt.axis('equal') #force square aspect ratio; this assuming X and Y are equal.
 plt.show()
 ##------------------------------------------------------------------
 ##------------------------------------------------------------------
