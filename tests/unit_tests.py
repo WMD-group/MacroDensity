0b2c1a5fbffdcfa825cf88f5b27f6bcd58fc924e
Keith Butler <k.t.butler@bath.ac.uk>
2017-11-02 15:52:02 +0000

    Potential reading and averaging tests

---
+++ tests/unit_tests.py
@@ -0,0 +1,66 @@
+from __future__ import print_function
+import unittest
+import sys, os
+import numpy as np
+
+sys.path.insert(1,'/Users/keith/SCRIPTS/MacroDensity/MacroDensity')
+import macrodensity as md
+
+test_dir = os.path.abspath(os.path.dirname(__file__))
+
+class test_reading_functions(unittest.TestCase):
+    ''' Test the code for reading in charge and density files'''
+
+    def test_read_vasp(self):
+        '''Test the function for reading CHGCAR/LOCPOT'''
+        charge, ngx, ngy, ngz, lattice = md.read_vasp_density('CHGCAR.test')
+        self.assertEqual(charge[0], -.76010173913E+01)
+        self.assertEqual(charge[56 * 56 * 56 -1], -4.4496715627)
+        self.assertEqual(lattice[0,0], 2.7150000)
+        self.assertEqual(ngx, 56)
+
+    def test_matrix_2_abc(self):
+        '''Test the function for converting the lattice to abc, alpha,beta, gamma format'''
+        lattice = np.asarray([[2.715, 2.715,0.],[0., 2.715, 2.715], [2.715, 0., 2.715]])
+        a,b,c,a_vec,b_vec,c_vec = md.matrix_2_abc(lattice)
+        self.assertAlmostEqual(a, 3.8395898218429529)
+        self.assertAlmostEqual(b, 3.8395898218429529)
+        self.assertAlmostEqual(c, 3.8395898218429529)
+
+    def test_density_2_grid(self):
+        '''Test the function for projecting the potential onto a grid'''
+        charge, ngx, ngy, ngz, lattice = md.read_vasp_density('CHGCAR.test')
+        grid_pot, electrons = md.density_2_grid(charge, ngx, ngy, ngz)
+        self.assertAlmostEqual(grid_pot[0,0,0],-.76010173913E+01)
+        self.assertAlmostEqual(grid_pot[55, 55, 55], -4.4496715627)
+        self.assertAlmostEqual(electrons, 8.00000, places=4)
+
+class test_averaging_functions(unittest.TestCase):
+    ''' Test the code for averaging the density'''
+    def test_planar_average(self):
+        test_grid = np.zeros(shape=(3,3,3))
+        for i in range(3):
+            test_grid[i,:,0] = float(i)
+        planar = md.planar_average(test_grid,3,3,3)
+        self.assertAlmostEqual(planar[0], 1.0)
+        planar = md.planar_average(test_grid, 3, 3, 3, axis='x')
+        self.assertAlmostEqual(planar[2], 0.66666667)
+
+    def test_cube_potential(self):
+        '''Test the cube_potential function'''
+        test_grid = np.zeros(shape=(5, 5, 5))
+        for i in range(5):
+            for j in range(5):
+                for k in range(5):
+                    test_grid[i,j,k] = float(i * j * k)
+
+        potential, variance = md.cube_potential([0,0,0],[0,0,0],[2,2,2],test_grid,5,5,5)
+        self.assertAlmostEqual(potential, 0.125)
+        self.assertAlmostEqual(variance, 0.109375)
+        potential, variance = md.cube_potential([1, 1, 1], [0, 0, 0], [2, 2, 2], test_grid, 5, 5, 5)
+        potential, variance = md.cube_potential([1, 1, 1], [0, 0, 0], [3, 3, 3], test_grid, 5, 5, 5)
+        self.assertAlmostEqual(potential, 1.0)
+        self.assertAlmostEqual(variance, 3.6296296296296298)
+
+if __name__ == '__main__':
+    unittest.main()
