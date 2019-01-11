7ad5665c58c53e2f030ef4e716608c458d6cc0e8
Keith Butler <keith.butler@stfc.ac.uk>
2019-01-11 15:30:46 +0000

    update email address

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ __author__ = "Keith T. Butler"
 __copyright__ = "Copyright Keith T. Butler (2013)"
 __version__ = "1.0"
 __maintainer__ = "Keith T. Butler"
-__email__ = "k.t.butler@bath.ac.uk"
-__date__ = "Aug 24 2017"
+__email__ = "keith.butler@stfc.ac.uk"
+__date__ = "Jan 11 2019"
 
 from setuptools import setup
 import os
@@ -27,11 +27,11 @@ if __name__ == "__main__":
         long_description=open(os.path.join(module_dir, 'README.md')).read(),
         url='https://github.com/WMD-group/MacroDensity',
         author='Keith T. Butler',
-        author_email='k.t.butler@bath.ac.uk',
+        author_email='keith.butler@stfc.ac.uk',
         license='GNU General Public License (GPL) v3',
         packages=['macrodensity'],
         zip_safe=False,
-        install_requires=['scipy','numpy','spglib','ase'],
+        install_requires=['scipy', 'numpy', 'spglib', 'ase'],
         classifiers=['Programming Language :: Python',
                      'Development Status :: 5 - Production/Stable',
                      'Intended Audience :: Science/Research',
