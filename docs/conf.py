a322a82402300b41b39a669296c005766f5e9cae
Calysta Tesiman <ct821@ic.ac.uk>
2023-08-16 17:08:16 +0100

    update

--- docs/conf.py
+++ docs/conf.py
@@ -53,7 +53,7 @@ myst_enable_extensions = [
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 html_theme = 'sphinx_book_theme' #'furo'
-
+html_title = "MacroDensity"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
